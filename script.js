6ea9b8051c1d34c0aea36a91edc401e45aceb32d
Prosper Nnadi <prospernnadi3@gmail.com>
2020-12-14 22:53:39 +0100

    Completed styling

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// const popup = document.querySelector('.popup');
-// const close_popup = document.querySelector('.close-popup');
-// const button = document.querySelector('.popup-btn');
-// const background = document.querySelector('.container');
+const popup = document.querySelector('.popup');
+const close_popup = document.querySelector('.close-popup');
+const button = document.querySelector('.popup-btn');
+const background = document.querySelector('.container');
 
-// const start_pop = button.addEventListener('click', () => {
-//     popup.style.display = "block";
-// });
+const start_pop = button.addEventListener('click', () => {
+    background.style.display = "block";
+});
 
-// const end_pop = close_popup.addEventListener('click', () => {
-//     popup.style.display = "none";
-// });
+const end_pop = close_popup.addEventListener('click', () => {
+    background.style.display = "none";
+});
 
 // const background_close = background.addEventListener('click', () => {
-//     popup.style.display = "none";
+//     background.style.display = "none";
 // });
