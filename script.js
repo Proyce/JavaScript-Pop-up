c71c5e83f7a367b7af8f5c4fe2f8a6d6eb81ade4
Prosper Nnadi <prospernnadi3@gmail.com>
2020-12-14 22:31:47 +0100

    Modified styling

---
+++ script.js
@@ -0,0 +1,16 @@
+// const popup = document.querySelector('.popup');
+// const close_popup = document.querySelector('.close-popup');
+// const button = document.querySelector('.popup-btn');
+// const background = document.querySelector('body');
+
+// const start_pop = button.addEventListener('click', () => {
+//     popup.style.display = "block";
+// });
+
+// const end_pop = close_popup.addEventListener('click', () => {
+//     popup.style.display = "none";
+// });
+
+// const background_close = background.addEventListener('click', () => {
+//     popup.style.display = "none";
+// });
